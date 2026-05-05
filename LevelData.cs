d5860675bdef36c2d789c7a5c85f21ae9f5ea2d0
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2026-05-05 18:40:24 +0300

    Update LevelData.cs

--- LevelData.cs
+++ LevelData.cs
@@ -7,4 +7,8 @@ public class LevelData : ScriptableObject
     public int rowCount;    // Satır sayısı
     public int columnCount; // Sütun sayısı
     public float timeLimit; // Bu seviye için verilen süre
+
+    [Header("Star Thresholds (Moves)")]
+    public int threeStarMoveLimit; // Örn: 4 kart için 2, 30 kart için 18
+    public int twoStarMoveLimit;   // Örn: 4 kart için 4, 30 kart için 25
 }
