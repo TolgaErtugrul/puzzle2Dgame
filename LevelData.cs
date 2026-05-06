435b2cc42bb06fbd21a56de16871f63b8ee9070f
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2026-05-06 17:18:55 +0300

    Update LevelData.cs

--- LevelData.cs
+++ LevelData.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewLevelData", menuName = "MemoryGame/Level Data")]
+[CreateAssetMenu(fileName = "NewLevelData", menuName = "Game/Level Data")]
 public class LevelData : ScriptableObject
 {
-    public int levelNumber;
-    public int rowCount;    // Satır sayısı
-    public int columnCount; // Sütun sayısı
-    public float timeLimit; // Bu seviye için verilen süre
+    public int levelID;
+    public int rowCount;
+    public int columnCount;
+    public float timeLimit;
+    public int bombPairID = -2; // -2: Yok, -1: Rastgele, 0+: Belirli ID
+    public int bonusPairID = -2;
 
     [Header("Star Thresholds (Moves)")]
-    public int threeStarMoveLimit; // Örn: 4 kart için 2, 30 kart için 18
-    public int twoStarMoveLimit;   // Örn: 4 kart için 4, 30 kart için 25
+    public int threeStarMoveLimit; // Örn: 10 hamle ve altı 3 yıldız
+    public int twoStarMoveLimit;   // Örn: 15 hamle ve altı 2 yıldız
 }
