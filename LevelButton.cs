b40e94c8e9844ab69f195ab3574d33fe6915fb87
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2026-05-06 17:22:25 +0300

    Update LevelButton.cs

--- LevelButton.cs
+++ LevelButton.cs
@@ -1,1 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
+public class LevelButton : MonoBehaviour
+{
+    public int levelIndex;
+    public TextMeshProUGUI levelNumberText;
+    public Image[] starIcons;
+    public GameObject lockIcon;
+    public Button button;
+
+    public void Setup(int index, int stars, bool isUnlocked)
+    {
+        levelIndex = index;
+        levelNumberText.text = (index + 1).ToString();
+        button.interactable = isUnlocked;
+        lockIcon.SetActive(!isUnlocked);
+
+        // Yıldızları göster
+        for (int i = 0; i < starIcons.Length; i++)
+        {
+            starIcons[i].color = (i < stars) ? Color.yellow : Color.gray;
+        }
+    }
+
+    public void OnClick()
+    {
+        // GameManager'a bu seviyeyi yüklemesini söyleyeceğiz
+        // SceneManager.LoadScene("GameScene"); gibi...
+    }
+}
