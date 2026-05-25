97e65c6e915c75fd5c2704703709e35a0fc6b4d0
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2026-05-25 12:57:25 +0300

    Feat: Add stylish market UI with inventory and purchase system

---
+++ MarketUIHandler.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MarketUIHandler : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public TextMeshProUGUI totalStarsText;
+    public TextMeshProUGUI extraTimeCountText;
+    public TextMeshProUGUI cancelBombCountText;
+
+    [Header("Buttons")]
+    public Button buyExtraTimeBtn;
+    public Button buyCancelBombBtn;
+
+    void Start()
+    {
+        UpdateMarketUI();
+    }
+
+    public void UpdateMarketUI()
+    {
+        // Toplam yıldızı güncelle
+        int totalStars = PlayerPrefs.GetInt("TotalStars", 0);
+        totalStarsText.text = totalStars.ToString();
+
+        // Envanter miktarlarını güncelle
+        extraTimeCountText.text = "Sahip olunan: " + PlayerPrefs.GetInt("Inventory_ExtraTime", 0);
+        cancelBombCountText.text = "Sahip olunan: " + PlayerPrefs.GetInt("Inventory_CancelBomb", 0);
+
+        // Butonların tıklanabilirliğini kontrol et (Yetersiz bakiye kontrolü)
+        buyExtraTimeBtn.interactable = totalStars >= ShopManager.EXTRA_TIME_PRICE;
+        buyCancelBombBtn.interactable = totalStars >= ShopManager.CANCEL_BOMB_PRICE;
+    }
+
+    public void OnClick_BuyExtraTime()
+    {
+        if (ShopManager.BuyExtraTime())
+        {
+            Debug.Log("Süre Satın Alındı!");
+            UpdateMarketUI();
+            // Buraya bir 'Satın Alındı' sesi veya efekti ekleyebilirsin
+        }
+    }
+
+    public void OnClick_BuyCancelBomb()
+    {
+        if (ShopManager.BuyCancelBomb())
+        {
+            Debug.Log("Bomba İptali Satın Alındı!");
+            UpdateMarketUI();
+        }
+    }
+}
