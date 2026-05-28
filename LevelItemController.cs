66a67d368195ebcd69cbe66e1a4020c36723910b
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2026-05-28 12:55:20 +0300

    Update level item panel with inventory counts and interactable buttons

--- LevelItemController.cs
+++ LevelItemController.cs
@@ -1,33 +1,47 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class LevelItemController : MonoBehaviour
 {
+    [Header("UI Elements")]
     public TextMeshProUGUI extraTimeStockText;
     public TextMeshProUGUI cancelBombStockText;
+    
+    [Header("Buttons")]
+    public Button extraTimeBtn;
+    public Button cancelBombBtn;
 
+    // Panel her SetActive(true) yapıldığında bu çalışır
     void OnEnable()
     {
-        // Panel her açıldığında stokları güncelle
-        UpdateStocks();
+        UpdateItemButtons();
     }
 
-    public void UpdateStocks()
+    public void UpdateItemButtons()
     {
-        extraTimeStockText.text = "x" + PlayerPrefs.GetInt("Inventory_ExtraTime", 0);
-        cancelBombStockText.text = "x" + PlayerPrefs.GetInt("Inventory_CancelBomb", 0);
+        int extraTimeCount = PlayerPrefs.GetInt("Inventory_ExtraTime", 0);
+        int cancelBombCount = PlayerPrefs.GetInt("Inventory_CancelBomb", 0);
+
+        // Metinleri güncelle
+        extraTimeStockText.text = "x" + extraTimeCount;
+        cancelBombStockText.text = "x" + cancelBombCount;
+
+        // Stok 0 ise butonu tıklanamaz yap, değilse aç
+        extraTimeBtn.interactable = extraTimeCount > 0;
+        cancelBombBtn.interactable = cancelBombCount > 0;
     }
 
-    // Butonlara basıldığında GameManager'daki ilgili fonksiyonları çağıracağız
     public void UseExtraTime()
     {
+        // GameManager'a "Hazırlan" komutu ver
         FindObjectOfType<GameManager>().PrepareExtraTime();
-        UpdateStocks();
+        UpdateItemButtons(); // Sayıyı düşürmek için UI'ı yenile
     }
 
     public void UseCancelBomb()
     {
         FindObjectOfType<GameManager>().PrepareCancelBomb();
-        UpdateStocks();
+        UpdateItemButtons();
     }
 }
